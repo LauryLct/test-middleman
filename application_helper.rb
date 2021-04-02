cf06893b0483f5cf9cd7134b86a401df5d5fa8d8
Laury Lecaut <laury.lecaut@gmail.com>
2021-04-02 19:33:33 +0200

    navbar

---
+++ application_helper.rb
@@ -0,0 +1,13 @@
+module ApplicationHelper
+  def retina_image_tag(default_name, options={})
+    retina_name = default_name.gsub(%r{\.\w+$}, '@2x\0')
+    image_tag(default_name, options.merge('data-interchange' => "[#{asset_path(retina_name)}, (retina)]"))
+  end
+
+  def svg(name)
+    root = Middleman::Application.root
+    file_path = "#{root}/source/images/#{name}.svg"
+    return File.read(file_path) if File.exists?(file_path)
+    '(not found)'
+  end
+end
